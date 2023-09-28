88ffbba28ffb5ba2a7412a7613c6bdda3ecf408b
tif <youfoundtiffany@gmail.com>
2023-09-28 16:49:36 -0600

    updates

--- geo-stache.client/src/components/EditStacheForm.vue
+++ geo-stache.client/src/components/EditStacheForm.vue
@@ -1,57 +1,56 @@
-    <!-- FIXME NOT YET FORMATTED AS AN EDIT FORM, STILL A COPY FROM THE CREATE FORM -->
 <template>
     <div>
-        <form v-if="activeStache" @submit.prevent="editStache()">
 
+        <form v-if="activeStache" @submit.prevent="editStache">
+            <h1 class="text-center">Stache Create Form</h1>
             <!-- Stache Name Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
+            <div class=" form-group">
                 <label for="stacheName">Stache Name:</label>
-                <input type="text" id="stacheName" v-model="stacheName" minlength="3" maxlength="50" required />
-            </div>
+                <input type="text" id="stacheName" v-model="stacheData.stacheName" minlength="2" maxlength="50" required
+                    class="form-control" />
 
-            <!-- Latitude Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
-                <label for="lat">Latitude</label>
-                <input type="text" id="lat" v-model="lat" minlength="3" maxlength="50" required />
             </div>
+            <!-- Stache Description Input -->
+            <div class="form-group">
+                <label for="description">Stache Description:</label>
+                <textarea id="description" v-model="stacheData.description" minlength="10" maxlength="1000" required
+                    class="form-control"></textarea>
 
-            <!-- Longitude Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
-                <label for="lng">Longitude</label>
-                <input type="text" id="lng" v-model="lng" minlength="3" maxlength="50" required />
+            </div>
+            <div class="row">
+                <!-- Latitude Input -->
+                <div class="form-group col-6">
+                    <label for="lat">Latitude:</label>
+                    <input type="text" id="lat" v-model="stacheData.lat" minlength="3" maxlength="50" required
+                        class="form-control" />
+
+                </div>
+                <!-- Longitude Input -->
+                <div class=" form-group col-6">
+                    <label for="lng">Longitude:</label>
+                    <input type="text" id="lng" v-model="stacheData.lng" minlength="3" maxlength="50" required
+                        class="form-control" />
+                </div>
             </div>
 
             <!-- Cover Image Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
+            <div class="form-group">
                 <label for="coverImg">Cover Image:</label>
-                <input type="text" id="coverImg" v-model="coverImg" minlength="5" :maxlength="255" required />
+                <input type="text" id="coverImg" v-model="stacheData.coverImg" minlength="5" :maxlength="1000" required
+                    class="form-control" />
             </div>
 
             <!-- Badge Image Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
-                <label for="coverImg">Badge Image:</label>
-                <input type="text" id="coverImg" v-model="coverImg" minlength="5" :maxlength="255" required />
-            </div>
-
-            <!-- Stache Description Input -->
-            <div class="form-group col-12 col-md-6 mb-3">
-                <label for="description">Stache Description:</label>
-                <textarea id="description" v-model="description" minlength="10" maxlength="1000" required></textarea>
-            </div>
-
-            <!-- Local Area Select -->
-            <div class="form-group col-12 col-md-6 mb-3">
-                <label for="locationTag">Local Area:</label>
-                <select id="locationTag" v-model="locationTag" required>
-                    <option value="Boise">Boise</option>
-                    <!-- Add more cities within a 50-mile radius of Boise, Idaho -->
-                </select>
+            <div class="form-group">
+                <label for="badgeImg">Badge Image:</label>
+                <input type="text" id="badgeImg" v-model="stacheData.badgeImg" minlength="5" :maxlength="1000" required
+                    class="form-control" />
             </div>
 
             <!-- Difficulty Select -->
-            <div class="form-group col-12 mb-3">
+            <div class="form-group">
                 <label for="difficulty">Difficulty:</label>
-                <select id="difficulty" v-model="difficulty" required>
+                <select id="difficulty" v-model="stacheData.difficulty" required class="form-control">
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -61,47 +60,76 @@
             </div>
 
             <!-- Hint Input -->
-            <div class="form-group col-12 mb-3 w-100">
+            <div class="form-group">
                 <label for="hint">Hint:</label>
-                <textarea id="hint" v-model="hint" minlength="10" maxlength="500" required></textarea>
+                <textarea id="hint" v-model="stacheData.hint" minlength="10" maxlength="500" required
+                    class="form-control"></textarea>
             </div>
-            <button>submit</button>
+
+            <button class="btn btn-primary mt-3">submit</button>
+
         </form>
     </div>
 </template>
 
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import Pop from '../utils/Pop.js';
 import { stachesService } from '../services/StachesService.js';
 import { Modal } from 'bootstrap';
 import { useRouter } from 'vue-router';
+import { AppState } from '../AppState.js';
 export default {
     setup() {
-        const eventData = ref({})
-        const router = useRouter()
+        const stacheData = ref({})
+        const today = new Date()
+        const route = useRoute()
+
         function resetForm() {
-            eventData.value = { type: '' }
+            stacheData.value = {}
         }
+
+        function setForm() {
+
+            if (AppState.activeEvent) {
+                stacheData.value = {
+                    name: AppState.activeStache.name,
+                    description: AppState.activeStache.description,
+                    lat: AppState.activeStache.lat,
+                    lng: AppState.activeStache.lng,
+                    hint: AppState.activeStache.hint,
+                    coverImg: AppState.activeStache.coverImg,
+                    badgeImg: AppState.activeStache.coverImg,
+                    difficulty: AppState.activeStache.difficulty,
+                }
+            }
+            else {
+                console.log('fast render')
+            }
+        }
+
         onMounted(() => {
             resetForm()
+            setForm()
         })
-        return {
-            eventData,
-            async createStache() {
-                try {
 
-                    let newEvent = await stachesService.createStache(eventData.value)
-                    Pop.toast('Stache Created', 'success')
-                    resetForm()
-                    Modal.getOrCreateInstance('#id').hide()
-                    // router.push({ name: 'Stache Details', params: { stachId: newStache.id } })
-                    // FIXME enter correct params
+        return {
+            today,
+            route,
+            activeStache: computed(() => AppState.activeStache),
+            stacheData,
+
+            async editStache() {
+                try {
+                    await stachesService.editStache(route.params.stachId, stacheData.value)
+                    Pop.success('Your Stache was updated!')
+                    Modal.getOrCreateInstance('#')
                 } catch (error) {
                     Pop.error(error)
                 }
             }
+
         }
     }
 };
