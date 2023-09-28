597383c1ef7685da783afee16ae13441512c4551
AxelArciniega <axelarciniega12@icloud.com>
2023-09-28 14:55:03 -0600

    added the create service

--- geo-stache.client/src/services/StachesService.js
+++ geo-stache.client/src/services/StachesService.js
@@ -1,3 +1,7 @@
+import { AppState } from "../AppState"
+import { Stache } from "../models/Stache"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
 class StachesService {
     // NOTE ⬇️this is the Active Stache renderd on the StacheDetailsPage
@@ -6,8 +10,11 @@ class StachesService {
 
     }
 
-    async createStache(){
-        console.log('creating stache')
+    async createStache(stacheData){
+        const res = await api.post('api/staches', stacheData)
+        logger.log(res.data)
+        const newStache = new Stache(res.data)
+        AppState.staches.unshift(newStache)
     }
 }
 export const stachesService = new StachesService()
