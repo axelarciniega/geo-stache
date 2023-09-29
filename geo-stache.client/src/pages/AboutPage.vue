bbe51f5ce43d93786a418f9e943ef291d88a8a2c
Camille Ivins <camillemivins@gmail.com>
2023-09-29 11:40:20 -0600

    PULL

--- geo-stache.client/src/pages/AboutPage.vue
+++ geo-stache.client/src/pages/AboutPage.vue
@@ -1,90 +1,121 @@
 <template>
-  <section class="container-fluid">
+  <section class="container">
     <section class="row">
+
       <div class="col-12 table-card align-items-center text-dark">
-        <div class="row">
-          <div class="col-4 p-1">
+        <div class="row align-items-center">
+          <div class="col-2 p-4">
             <img class="profile-pic"
               src="https://images.unsplash.com/photo-1547809397-e2c7eea071fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
               alt="">
 
           </div>
-          <div class="col-7 px-1">
+          <div class="col-9 px-1">
             <p class="text-start fw-bold m-0">Tyler</p>
-            Beard wax? Nah, it's MOUSTACHE wax!
-            <div class="text-end">
-              <i class="mdi mdi-github fs-1"></i>
-              <i class="mdi mdi-linkedin fs-1"></i>
-            </div>
-          </div>
+            Beard wax? Nah, it's MOUSTACHE wax! Stachecraft veteran seeking to bring the adventure of the stache to the
+            masses, Tyler excels in drawing out the true outdoorsmen to the world of staching. Rain or shine, Expect to
+            find Tyler far from the concrete jungle, hiding some of the most difficult finds.
 
+          </div>
+          <div class="col-1">
+            <i class="mdi mdi-github fs-1"></i>
+            <i class="mdi mdi-linkedin fs-1"></i>
+          </div>
         </div>
 
       </div>
-      <!--
-        <div class="row table-card align-items-center text-dark">
-     
-          <div class="col-2 p-1"><img class="profile-pic"
+
+      <div class="col-12 table-card align-items-center text-dark">
+        <div class="row align-items-center">
+          <div class="col-2 p-4">
+            <img class="profile-pic"
               src="https://media.istockphoto.com/id/515399607/photo/close-up-of-young-man-with-long-moustaches.webp?b=1&s=170667a&w=0&k=20&c=c_Vs53atn9yVfi_JPw_61z6mDidu-1e3euNMGmtR5vg="
               alt="">
-            <p class="text-center fw-bold m-0">Axel</p>
+
           </div>
-          <div class="col-10 px-1">You can't handle these handlebars
-            <div class="text-end">
-              <i class="mdi mdi-github fs-1"></i>
-              <i class="mdi mdi-linkedin fs-1"></i>
-            </div>
+          <div class="col-9 px-1">
+            <p class="text-start fw-bold m-0">Axel</p>
+            You can't handle these handlebars! King of speedstaching, Axel is a renown stacheier and stachier supplier.
+            Featured in Stachunds of the West, Axel has insights to provide to both novice and expert Stachers, alike.
+
           </div>
-        
+          <div class="col-1">
+            <a class="mdi mdi-github fs-1 text-dark" href="https://github.com/axelarciniega"></a>
+            <i class="mdi mdi-linkedin fs-1"></i>
+          </div>
         </div>
 
-        <div class="row table-card align-items-center text-dark">
-  
-          <div class="col-2 p-1"><img class="profile-pic"
-              src="https://plus.unsplash.com/premium_photo-1661720137713-a85a195e0b1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-              alt="">
-            <p class="text-center fw-bold m-0">Sam</p>
-          </div>
-          <div class="col-10 px-1">I promise this is a bubble pipe
-            <div class="text-end">
-              <i class="mdi mdi-github fs-1"></i>
-              <i class="mdi mdi-linkedin fs-1"></i>
-            </div>
-          </div>
-          
-        </div>
-
-        <div class="row table-card align-items-center text-dark">
-          
-          <div class="col-2 p-1"><img class="profile-pic"
-              src="https://images.unsplash.com/photo-1588255890583-49e168f61999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
-              alt="">
-            <p class="text-center fw-bold m-0">Tiffany</p>
-          </div>
-          <div class="col-10 px-1">Art of the 'stache is the pinnacle of class
-            <div class="text-end">
-              <i class="mdi mdi-github fs-1"></i>
-              <i class="mdi mdi-linkedin fs-1"></i>
-            </div>
-          </div>
-          
-        </div>
-
-        <div class="row table-card align-items-center text-dark">
-          
-          <div class="col-2 p-1"><img class="profile-pic"
-              src="https://images.unsplash.com/photo-1534309466160-70b22cc6f854?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"
-              alt="">
-            <p class="text-center fw-bold m-0">Camille</p>
-          </div>
-          <div class="col-10 px-1">It's all in the twirl
-            <div class="text-end">
-              <i class="mdi mdi-github fs-1"></i>
-              <i class="mdi mdi-linkedin fs-1"></i>
-            </div>
       </div>
 
-      </div> -->
+      <div class="col-12 table-card align-items-center text-dark">
+        <div class="row align-items-center">
+          <div class="col-2 p-4">
+            <img class="profile-pic"
+              src="https://plus.unsplash.com/premium_photo-1661720137713-a85a195e0b1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+              alt="">
+
+          </div>
+          <div class="col-9 px-1">
+            <p class="text-start fw-bold m-0">Sam</p>
+            I promise this is a bubble pipe. Codestache pilot extraordinaire, Sam leverages both geologic and geopoetry to
+            cracking the most quixotic of hints. Elite Stachers are welcome to challenge Sam to a Stache-off, but be
+            warned - Sam has never lost.
+
+          </div>
+          <div class="col-1">
+            <i class="mdi mdi-github fs-1"></i>
+            <i class="mdi mdi-linkedin fs-1"></i>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-12 table-card align-items-center text-dark">
+        <div class="row align-items-center">
+          <div class="col-2 p-4">
+            <img class="profile-pic"
+              src="https://images.unsplash.com/photo-1588255890583-49e168f61999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+              alt="">
+
+          </div>
+          <div class="col-9 px-1">
+            <p class="text-start fw-bold m-0">Tiffany</p>
+            Art of the 'stache is the pinnacle of class. Queen Stache Tiffany has broadened the horizons of stache
+            culture. Responsible for bringing stachey back, Tiffany works tirelessly to make staching an activity
+            attractive to the people.
+
+          </div>
+          <div class="col-1">
+            <i class="mdi mdi-github fs-1"></i>
+            <i class="mdi mdi-linkedin fs-1"></i>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-12 table-card align-items-center text-dark">
+        <div class="row align-items-center">
+          <div class="col-2 p-4">
+            <img class="profile-pic"
+              src="https://images.unsplash.com/photo-1534309466160-70b22cc6f854?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"
+              alt="">
+
+          </div>
+          <div class="col-9 px-1">
+            <p class="text-start fw-bold m-0">Camille</p>
+            It's all in the twirl. Stealthstacher VII, Camille has attainded the highest rank within the Stachers of
+            Subtility. Committed to maintaining pervasive wonder in the ordinary, Camille's stachepertise is focused on
+            elevating the game for the most dedicated of stachers.
+
+          </div>
+          <div class="col-1">
+            <i class="mdi mdi-github fs-1"></i>
+            <i class="mdi mdi-linkedin fs-1"></i>
+          </div>
+        </div>
+
+      </div>
+
 
     </section>
   </section>
@@ -101,12 +132,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// .profile-pic {
+
+//   border-radius: 50%;
+//   width: 100%;
+//   object-fit: cover;
+//   object-position: center;
+// }
+
 .profile-pic {
-  max-height: 10vh;
+  // border-top-left-radius: 15px;
+  // border-bottom-right-radius: 15px;
   border-radius: 50%;
   width: 100%;
-  object-fit: contain;
+  aspect-ratio: 1/1;
+  object-fit: cover;
   object-position: center;
+  // elevation-2 settings
+  box-shadow: 0 3px 3px -1px rgba(205, 205, 205, 0.2),
+    0 5px 6px 0 rgba(205, 205, 205, 0.14),
+    0 1px 8px 0 rgba(205, 205, 205, 0.12);
 }
 
 .table-card {
