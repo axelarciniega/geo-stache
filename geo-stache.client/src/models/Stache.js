67fa018d1cba5953f6cd1d76136c4da7b0f5565a
TylerHigby <tyhigby22@msn.com>
2023-09-29 14:06:51 -0600

    asdfas

--- geo-stache.client/src/models/Stache.js
+++ geo-stache.client/src/models/Stache.js
@@ -13,6 +13,6 @@ export class Stache {
         this.lat = data.location.coordinates ? data.location.coordinates[0] : null
         this.lng = data.location.coordinates ? data.location.coordinates[1] : null
         // NOTE this is used by (browser Navigator and is the intersection of the lat and lng. Location is a keyword, do not use by itself as a name.)
-        this.stacheLocation= data.location
+        this.stacheLocation = data.location
     }
 }
