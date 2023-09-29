db24ca1127162c362dab65a1b8e91480681377d7
tif <youfoundtiffany@gmail.com>
2023-09-29 10:07:12 -0600

    bgs

--- geo-stache.client/src/App.vue
+++ geo-stache.client/src/App.vue
@@ -7,22 +7,20 @@
     <main>
       <router-view />
     </main>
-<<<<<<< HEAD
-    <!-- NOTE FOOTER -->
-=======
-<<<<<<< HEAD
-    <footer class="bg-dark text-light">
-      <!-- Made with 🥸 by Geo-Stache -->
-      <img class="footer-bg" src="./assets/img/TheStache.png" alt="">
-=======
->>>>>>> 00811adc015d62c3f6b89e1ba8f4e01236587f9f
-    <footer class="text-light"><span>
+    <<<<<<< HEAD <!-- NOTE FOOTER -->
+      =======
+      <<<<<<< HEAD <footer class="bg-dark text-light">
+        <!-- Made with 🥸 by Geo-Stache -->
+        <img class="footer-bg" src="./assets/img/TheStache.png" alt="">
+        =======
+        >>>>>>> 00811adc015d62c3f6b89e1ba8f4e01236587f9f
+        <footer class="text-light"><span>
 
-        <img :src="blackStacheImage" alt="Geo-Stache Logo" class="stache text-center">
+            <img :src="blackStacheImage" alt="Geo-Stache Logo" class="stache text-center">
 
-      </span>
->>>>>>> 025bd5e36a7da95156e582bff058305b343cee8b
-    </footer>
+          </span>
+          >>>>>>> 025bd5e36a7da95156e582bff058305b343cee8b
+        </footer>
 
   </body>
 </template>
@@ -49,8 +47,11 @@ export default {
 .bg-img {
   // FIXME Uncomment after change text color
   // background-image: url(https://images.unsplash.com/photo-1554755229-ca4470e07232?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80);
-  background-image: url("https://images.unsplash.com/photo-1520299607509-dcd935f9a839?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80");
-  // background-image: url("./assets/img/Geo-Stache-bg-Copy.png");
+  // background-image: url("https://images.unsplash.com/photo-1520299607509-dcd935f9a839?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80");
+  // background-image: url("./assets/img/bg-map-75.png");
+  // background-image: url("./assets/img/bg-map-60.png");
+  // background-image: url("./assets/img/bg-map-50.png");
+  background-image: url("./assets/img/bg-map-40.png");
   object-position: right;
   object-fit: cover;
   background-attachment: fixed;
