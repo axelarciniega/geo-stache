39075257bf648073b940cf808eb44e0b029e2ab6
Camille Ivins <camillemivins@gmail.com>
2023-09-29 10:45:06 -0600

    Account, sync

--- geo-stache.client/src/pages/AboutPage.vue
+++ geo-stache.client/src/pages/AboutPage.vue
@@ -1,87 +1,90 @@
 <template>
   <section class="container-fluid">
-    <section>
-      <div class="table-card align-items-center text-dark">
-        <!-- This will be filled proper later -->
-        <div class="col-2 p-1">
-          <p class="text-center fw-bold m-0">Tyler</p>
-          <img class="profile-pic"
-            src="https://images.unsplash.com/photo-1547809397-e2c7eea071fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-            alt="">
+    <section class="row">
+      <div class="col-12 table-card align-items-center text-dark">
+        <div class="row">
+          <div class="col-4 p-1">
+            <img class="profile-pic"
+              src="https://images.unsplash.com/photo-1547809397-e2c7eea071fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+              alt="">
+
+          </div>
+          <div class="col-7 px-1">
+            <p class="text-start fw-bold m-0">Tyler</p>
+            Beard wax? Nah, it's MOUSTACHE wax!
+            <div class="text-end">
+              <i class="mdi mdi-github fs-1"></i>
+              <i class="mdi mdi-linkedin fs-1"></i>
+            </div>
+          </div>
 
         </div>
-        <span class="col-9 px-1">Beard wax? Nah, it's MOUSTACHE wax!
-          <div class="text-end">
-            <i class="mdi mdi-github fs-1"></i>
-            <i class="mdi mdi-linkedin fs-1"></i>
+
+      </div>
+      <!--
+        <div class="row table-card align-items-center text-dark">
+     
+          <div class="col-2 p-1"><img class="profile-pic"
+              src="https://media.istockphoto.com/id/515399607/photo/close-up-of-young-man-with-long-moustaches.webp?b=1&s=170667a&w=0&k=20&c=c_Vs53atn9yVfi_JPw_61z6mDidu-1e3euNMGmtR5vg="
+              alt="">
+            <p class="text-center fw-bold m-0">Axel</p>
           </div>
-        </span>
-        <!-- <td class="col-1 px-1">Germany</td> -->
+          <div class="col-10 px-1">You can't handle these handlebars
+            <div class="text-end">
+              <i class="mdi mdi-github fs-1"></i>
+              <i class="mdi mdi-linkedin fs-1"></i>
+            </div>
+          </div>
+        
+        </div>
+
+        <div class="row table-card align-items-center text-dark">
+  
+          <div class="col-2 p-1"><img class="profile-pic"
+              src="https://plus.unsplash.com/premium_photo-1661720137713-a85a195e0b1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+              alt="">
+            <p class="text-center fw-bold m-0">Sam</p>
+          </div>
+          <div class="col-10 px-1">I promise this is a bubble pipe
+            <div class="text-end">
+              <i class="mdi mdi-github fs-1"></i>
+              <i class="mdi mdi-linkedin fs-1"></i>
+            </div>
+          </div>
+          
+        </div>
+
+        <div class="row table-card align-items-center text-dark">
+          
+          <div class="col-2 p-1"><img class="profile-pic"
+              src="https://images.unsplash.com/photo-1588255890583-49e168f61999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+              alt="">
+            <p class="text-center fw-bold m-0">Tiffany</p>
+          </div>
+          <div class="col-10 px-1">Art of the 'stache is the pinnacle of class
+            <div class="text-end">
+              <i class="mdi mdi-github fs-1"></i>
+              <i class="mdi mdi-linkedin fs-1"></i>
+            </div>
+          </div>
+          
+        </div>
+
+        <div class="row table-card align-items-center text-dark">
+          
+          <div class="col-2 p-1"><img class="profile-pic"
+              src="https://images.unsplash.com/photo-1534309466160-70b22cc6f854?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"
+              alt="">
+            <p class="text-center fw-bold m-0">Camille</p>
+          </div>
+          <div class="col-10 px-1">It's all in the twirl
+            <div class="text-end">
+              <i class="mdi mdi-github fs-1"></i>
+              <i class="mdi mdi-linkedin fs-1"></i>
+            </div>
       </div>
 
-      <div class="row table-card align-items-center text-dark">
-        <!-- This will be filled proper later -->
-        <div class="col-2 p-1"><img class="profile-pic"
-            src="https://media.istockphoto.com/id/515399607/photo/close-up-of-young-man-with-long-moustaches.webp?b=1&s=170667a&w=0&k=20&c=c_Vs53atn9yVfi_JPw_61z6mDidu-1e3euNMGmtR5vg="
-            alt="">
-          <p class="text-center fw-bold m-0">Axel</p>
-        </div>
-        <div class="col-10 px-1">You can't handle these handlebars
-          <div class="text-end">
-            <i class="mdi mdi-github fs-1"></i>
-            <i class="mdi mdi-linkedin fs-1"></i>
-          </div>
-        </div>
-        <!-- <td class="col-1 px-1">Germany</td> -->
-      </div>
-
-      <div class="row table-card align-items-center text-dark">
-        <!-- This will be filled proper later -->
-        <div class="col-2 p-1"><img class="profile-pic"
-            src="https://plus.unsplash.com/premium_photo-1661720137713-a85a195e0b1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-            alt="">
-          <p class="text-center fw-bold m-0">Sam</p>
-        </div>
-        <div class="col-10 px-1">I promise this is a bubble pipe
-          <div class="text-end">
-            <i class="mdi mdi-github fs-1"></i>
-            <i class="mdi mdi-linkedin fs-1"></i>
-          </div>
-        </div>
-        <!-- <td class="col-1 px-1">Germany</td> -->
-      </div>
-
-      <div class="row table-card align-items-center text-dark">
-        <!-- This will be filled proper later -->
-        <div class="col-2 p-1"><img class="profile-pic"
-            src="https://images.unsplash.com/photo-1588255890583-49e168f61999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
-            alt="">
-          <p class="text-center fw-bold m-0">Tiffany</p>
-        </div>
-        <div class="col-10 px-1">Art of the 'stache is the pinnacle of class
-          <div class="text-end">
-            <i class="mdi mdi-github fs-1"></i>
-            <i class="mdi mdi-linkedin fs-1"></i>
-          </div>
-        </div>
-        <!-- <td class="col-1 px-1">Germany</td> -->
-      </div>
-
-      <div class="row table-card align-items-center text-dark">
-        <!-- This will be filled proper later -->
-        <div class="col-2 p-1"><img class="profile-pic"
-            src="https://images.unsplash.com/photo-1534309466160-70b22cc6f854?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80"
-            alt="">
-          <p class="text-center fw-bold m-0">Camille</p>
-        </div>
-        <div class="col-10 px-1">It's all in the twirl
-          <div class="text-end">
-            <i class="mdi mdi-github fs-1"></i>
-            <i class="mdi mdi-linkedin fs-1"></i>
-          </div>
-        </div>
-        <!-- <td class="col-1 px-1">Germany</td> -->
-      </div>
+      </div> -->
 
     </section>
   </section>
@@ -99,9 +102,10 @@ export default {
 
 <style lang="scss" scoped>
 .profile-pic {
-  border-radius: 15px;
+  max-height: 10vh;
+  border-radius: 50%;
   width: 100%;
-  object-fit: cover;
+  object-fit: contain;
   object-position: center;
 }
 
