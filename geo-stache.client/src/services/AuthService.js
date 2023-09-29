dd3f3468fa834b152eea8ed81637895bcfb8b397
TylerHigby <tyhigby22@msn.com>
2023-09-29 11:10:52 -0600

    auth0

--- geo-stache.client/src/services/AuthService.js
+++ geo-stache.client/src/services/AuthService.js
@@ -27,7 +27,7 @@ AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async function() {
   AppState.user = AuthService.user
   await accountService.getAccount()
   socketService.authenticate(AuthService.bearer)
-  await stachesService.getStaches()
+  // await stachesService.getStaches()
   // NOTE if there is something you want to do once the user is authenticated, place that here
 })
 
