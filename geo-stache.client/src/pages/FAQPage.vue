a12d152dbb61d3851be216a605824899cff32e14
tif <youfoundtiffany@gmail.com>
2023-09-30 19:59:13 -0600

    faq

--- geo-stache.client/src/pages/FAQPage.vue
+++ geo-stache.client/src/pages/FAQPage.vue
@@ -1,110 +1,122 @@
 <template>
-    <div class="container">
-        <div class="col-md-9 col-12 px-1 txt-DrkGreen fnt-Cabin">
-            <h2>Frequently Asked Questions (FAQ)</h2>
-            <ul>
-                <li class="card bg-Sand">
-                    <strong>What is Geo-Stache?</strong>
-                    <p>
-                        Geo-Stache is an innovative geo-staching experience that combines technology, open-source
-                        collaboration,
-                        and exploration. It allows users to engage in treasure hunting activities without the need for
-                        physical
-                        containers, making it accessible to a broader audience.
-                    </p>
-                </li>
-                <li>
-                    <strong>How do I get started with Geo-Stache?</strong>
-                    <p>
-                        To get started with Geo-Stache, simply create an account on our platform. Once registered, you can
-                        use
-                        the
-                        Geo-Stache app or website to explore geo-staches, participate in challenges, and collaborate with
-                        other
-                        enthusiasts.
-                    </p>
-                </li>
-                <li>
-                    <strong>Is Geo-Stache open source?</strong>
-                    <p>
-                        Yes, Geo-Stache is entirely open source. We believe in fostering a global community of geo-stachers
-                        who
-                        can
-                        contribute to and enhance the Geo-Stache experience. You can access the source code, participate in
-                        development,
-                        and
-                        even create your own geo-staching challenges.
-                    </p>
-                </li>
-                <li>
-                    <strong>Do I need any special equipment to play Geo-Stache?</strong>
-                    <p>
-                        Geo-Stache can be played with a smartphone or GPS-enabled device, as well as a stable internet
-                        connection. No
-                        special equipment or physical caches are required, making it accessible to virtually anyone.
-                    </p>
-                </li>
-                <li>
-                    <strong>How do I find geo-staches on Geo-Stache?</strong>
-                    <p>
-                        Using the Geo-Stache mobile friendly website, you can browse the map to discover geo-staches near
-                        your
-                        location.
-                        Simply select a geo-stache, navigate to its coordinates, and engage in the challenge or adventure it
-                        offers.
-                    </p>
-                </li>
-                <li>
-                    <strong>Can I create my own geo-staches on Geo-Stache?</strong>
-                    <p>
-                        Absolutely! Geo-Stache encourages users to create their own geo-staches and challenges. You can
-                        design
-                        unique
-                        adventures, set coordinates, and invite others to participate. It's a platform for geo-staching
-                        enthusiasts to
-                        unleash their creativity.
-                    </p>
-                </li>
-                <li>
-                    <strong>Is Geo-Stache suitable for beginners?</strong>
-                    <p>
-                        Yes, Geo-Stache is suitable for geo-staching beginners and experienced enthusiasts alike. The
-                        digital
-                        nature
-                        of
-                        the platform simplifies the experience, making it user-friendly for those new to geo-staching while
-                        offering
-                        exciting challenges for veterans.
-                    </p>
-                </li>
-                <li>
-                    <strong>Are there any costs associated with using Geo-Stache?</strong>
-                    <p>
-                        Geo-Stache is committed to being accessible to all. The Geo-Stache experience is designed to be
-                        inclusive and
-                        budget-friendly.
-                    </p>
-                </li>
-                <li>
-                    <strong>How do I connect with other geo-stachers on Geo-Stache?</strong>
-                    <p>
-                        Geo-Stache provides social features that allow you to connect with other geo-stachers. You can join
-                        communities,
-                        share your adventures, and collaborate on challenges, fostering a sense of camaraderie among users.
-                    </p>
-                </li>
-                <li>
-                    <strong>Is Geo-Stache available on mobile devices?</strong>
-                    <p>
-                        Yes, Geo-Stache offers a mobile-friendly web app.
-                    </p>
-                </li>
-            </ul>
+    <div>
+        <section class="container">
 
-            <router-link :to="{ name: 'Why' }">
-                <div class="fs-3">Learn more about Geo-Stache</div>
-            </router-link>
-        </div>
+            <div class="row txt-DrkGreen fnt-Cabin p-3">
+                <h2>Frequently Asked Questions (FAQ)</h2>
+                <ul>
+                    <li class="rounded ps-2 py-1 pe-1 m-3 table-card">
+                        <strong>What is Geo-Stache?</strong>
+                        <p>
+                            Geo-Stache is an innovative geo-staching experience that combines technology, open-source
+                            collaboration,
+                            and exploration. It allows users to engage in treasure hunting activities without the need for
+                            physical
+                            containers, making it accessible to a broader audience.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>How do I get started with Geo-Stache?</strong>
+                        <p>
+                            To get started with Geo-Stache, simply create an account on our platform. Once registered, you
+                            can
+                            use
+                            the
+                            Geo-Stache app or website to explore geo-staches, participate in challenges, and collaborate
+                            with
+                            other
+                            enthusiasts.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>Is Geo-Stache open source?</strong>
+                        <p>
+                            Yes, Geo-Stache is entirely open source. We believe in fostering a global community of
+                            geo-stachers
+                            who
+                            can
+                            contribute to and enhance the Geo-Stache experience. You can access the source code, participate
+                            in
+                            development,
+                            and
+                            even create your own geo-staching challenges.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>Do I need any special equipment to play Geo-Stache?</strong>
+                        <p>
+                            Geo-Stache can be played with a smartphone or GPS-enabled device, as well as a stable internet
+                            connection. No
+                            special equipment or physical caches are required, making it accessible to virtually anyone.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3 table-card">
+                        <strong>How do I find geo-staches on Geo-Stache?</strong>
+                        <p>
+                            Using the Geo-Stache mobile friendly website, you can browse the map to discover geo-staches
+                            near
+                            your
+                            location.
+                            Simply select a geo-stache, navigate to its coordinates, and engage in the challenge or
+                            adventure it
+                            offers.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3 table-card">
+                        <strong>Can I create my own geo-staches on Geo-Stache?</strong>
+                        <p>
+                            Absolutely! Geo-Stache encourages users to create their own geo-staches and challenges. You can
+                            design
+                            unique
+                            adventures, set coordinates, and invite others to participate. It's a platform for geo-staching
+                            enthusiasts to
+                            unleash their creativity.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>Is Geo-Stache suitable for beginners?</strong>
+                        <p>
+                            Yes, Geo-Stache is suitable for geo-staching beginners and experienced enthusiasts alike. The
+                            digital
+                            nature
+                            of
+                            the platform simplifies the experience, making it user-friendly for those new to geo-staching
+                            while
+                            offering
+                            exciting challenges for veterans.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3 table-card">
+                        <strong>Are there any costs associated with using Geo-Stache?</strong>
+                        <p>
+                            Geo-Stache is committed to being accessible to all. The Geo-Stache experience is designed to be
+                            inclusive and
+                            budget-friendly.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>How do I connect with other geo-stachers on Geo-Stache?</strong>
+                        <p>
+                            Geo-Stache provides social features that allow you to connect with other geo-stachers. You can
+                            join
+                            communities,
+                            share your adventures, and collaborate on challenges, fostering a sense of camaraderie among
+                            users.
+                        </p>
+                    </li>
+                    <li class="rounded ps-2 py-1 pe-1 m-3  table-card">
+                        <strong>Is Geo-Stache available on mobile devices?</strong>
+                        <p>
+                            Yes, Geo-Stache offers a mobile-friendly web app.
+                        </p>
+                    </li>
+                </ul>
+
+                <router-link :to="{ name: 'Why' }">
+                    <div class="fs-3">Learn more about Geo-Stache</div>
+                </router-link>
+            </div>
+        </section>
     </div>
 </template>
 
--- geo-stache.client/src/pages/WhyGeoStachePage.vue
+++ geo-stache.client/src/pages/WhyGeoStachePage.vue
@@ -1,25 +1,53 @@
 <template>
-    <div class="p-2 fs-5">
-        <p>We're excited to introduce Geo-Stache, a groundbreaking open-source geocache activity that takes the thrill of
-            treasure hunting to new heights. With Geo-Stache, we've combined the principles of geocaching with a fresh and
-            communal approach. By embracing open source, we're making it easy for anyone to participate and contribute to
-            this exciting adventure. Geo-Stache is more than just a treasure hunt; it's a platform for enthusiasts to
-            collaborate, share, and create their own geocaching experiences. Join us in this exciting journey of
-            exploration, where the thrill of discovery meets the power of open collaboration. Whether you're an experienced
-            cacher or a newcomer to the world of geocaching, Geo-Stache welcomes you to embark on a global treasure hunt
-            like never before.</p>
-        <p>
-            Innovating the geocaching experience, we've removed the traditional necessity of physical containers, or caches,
-            in Geo-Stache. By harnessing the capabilities of modern technology and embracing a digital-first approach, we've
-            made it possible for geocachers to participate without the need for physical items. With Geo-Stache, the world
-            becomes your canvas, and geocaches are represented virtually, allowing for endless possibilities and challenges.
-            This evolution not only simplifies the logistics of the game but also opens up opportunities for creativity and
-            engagement like never before. Now, you can explore and interact with geocaches seamlessly in the digital realm,
-            fostering a dynamic and accessible geocaching community for all. Join us in redefining the future of treasure
-            hunting with Geo-Stache, where the adventure is boundless, and the cache is no longer confined to the physical
-            world.
-        </p>
-    </div>
+    <section class="container">
+
+        <div class="p-2 fs-5">
+            <div class="rounded ps-2 py-1 pe-1 m-3 table-card">
+                <div>
+                    <strong class="text-center">Why Geo-Stache?</strong>
+                </div>
+
+                <p>We're excited to introduce Geo-Stache, a groundbreaking open-source geocache activity that takes the
+                    thrill
+                    of
+                    treasure hunting to new heights. With Geo-Stache, we've combined the principles of geocaching with a
+                    fresh
+                    and
+                    communal approach. By embracing open source, we're making it easy for anyone to participate and
+                    contribute
+                    to
+                    this exciting adventure. Geo-Stache is more than just a treasure hunt; it's a platform for enthusiasts
+                    to
+                    collaborate, share, and create their own geocaching experiences. Join us in this exciting journey of
+                    exploration, where the thrill of discovery meets the power of open collaboration. Whether you're an
+                    experienced
+                    cacher or a newcomer to the world of geocaching, Geo-Stache welcomes you to embark on a global treasure
+                    hunt
+                    like never before.</p>
+                <p>
+                    Innovating the geocaching experience, we've removed the traditional necessity of physical containers, or
+                    caches,
+                    in Geo-Stache. By harnessing the capabilities of modern technology and embracing a digital-first
+                    approach,
+                    we've
+                    made it possible for geocachers to participate without the need for physical items. With Geo-Stache, the
+                    world
+                    becomes your canvas, and geocaches are represented virtually, allowing for endless possibilities and
+                    challenges.
+                    This evolution not only simplifies the logistics of the game but also opens up opportunities for
+                    creativity
+                    and
+                    engagement like never before. Now, you can explore and interact with geocaches seamlessly in the digital
+                    realm,
+                    fostering a dynamic and accessible geocaching community for all. Join us in redefining the future of
+                    treasure
+                    hunting with Geo-Stache, where the adventure is boundless, and the cache is no longer confined to the
+                    physical
+                    world.
+                </p>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -31,4 +59,36 @@ export default {
 </script>
 
 
-<style></style>
+
+<style lang="scss" scoped>
+// .profile-pic {
+
+//   border-radius: 50%;
+//   width: 100%;
+//   object-fit: cover;
+//   object-position: center;
+// }
+
+.profile-pic {
+    // border-top-left-radius: 15px;
+    // border-bottom-right-radius: 15px;
+    border-radius: 50%;
+    width: 100%;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    object-position: center;
+    // elevation-2 settings
+
+}
+
+.table-card {
+    backdrop-filter: blur(20px);
+    // background-color: #e3d4bf;
+    border-radius: 5px;
+
+    width: 100%;
+    box-shadow: 0 1px 1px -1px rgba(133, 133, 133, 0.85),
+        0 1px 8px 0 rgba(133, 133, 133, 0.79),
+        0 1px 10px 0 rgba(133, 133, 133, 0.79);
+}
+</style>
