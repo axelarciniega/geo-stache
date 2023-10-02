05dd6f638bb30178e152f8de08a8b1b843b638d1
AxelArciniega <axelarciniega12@icloud.com>
2023-10-02 15:31:05 -0600

    pushing

---
+++ geo-stache.client/src/components/ActiveProfileStacheCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <router-link :to="{path: `staches/${stache.id}`}">
+            {{ stache.stacheName }}
+        </router-link>
+    </section>
+</template>
+
+<script>
+import { Stache } from '../models/Stache';
+
+
+
+
+
+export default {
+    
+    props: {stache: {type: Stache || Object, required: true}},
+setup() {
+    
+  return {};
+},
+};
+</script>
+
+
+<style>
+</style>
