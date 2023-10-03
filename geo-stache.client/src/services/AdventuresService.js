9dbf4d60b76489b6ec61c174604dbf82bec53f07
tif <youfoundtiffany@gmail.com>
2023-10-03 16:43:55 -0600

    EOD updates

--- geo-stache.client/src/services/AdventuresService.js
+++ geo-stache.client/src/services/AdventuresService.js
@@ -28,14 +28,19 @@ class AdventuresService {
     }
 
 
-    // completeAdventure(adventureId){
+    // async completeAdventure(stacheId) {
+    //     let completedAdventure = AppState.adventures.find(a => a.id == stacheId)
+
+    //     const response = await api.put(`api/adventures/${adventureId}`)
+    //     logger.log(response.data)
+
+    // }
+
+
     // TODO find adventure in app state
     // TODO change adventure.status = 'completed'
     // TODO change adventure.foundDate = Date.now()
     // TODO send put request ('api/adventures/adventureId, foundadventure)
     // TODO  do a splice to get reactive change to see that flip on the page
-    // }
-
-
 }
 export const adventuresService = new AdventuresService()
