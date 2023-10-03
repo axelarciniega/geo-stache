3469595fe0c6b64568eab33960ff53c3c94b2a9b
bluebear2015 <samluna61091@gmail.com>
2023-10-03 13:29:55 -0600

    commit

--- geo-stache.client/src/components/ToDoCard.vue
+++ geo-stache.client/src/components/ToDoCard.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <div v-show="adventure.status === 'todo'">
-            {{ adventure.stache.stacheName }}
-        </div>
-
+        <router-link :to="{ path: `staches/${adventure.stacheId}` }">
+            <div v-show="adventure.status === 'todo'">
+                {{ adventure.stache.stacheName }}
+                {{ adventure.stacheId }}
+            </div>
+        </router-link>
     </div>
 </template>
 
 
 
 <script>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
 import { Adventure } from '../models/Adventure';
 
 Adventure
@@ -18,7 +22,9 @@ export default {
         adventure: { type: Adventure || Object, required: true }
     },
     setup() {
-        return {};
+        return {
+
+        };
     },
 };
 </script>
