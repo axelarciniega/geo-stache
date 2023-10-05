577a7f246dabfc6ec738983708d302ffdef1b384
Camille Ivins <camillemivins@gmail.com>
2023-10-05 11:19:36 -0600

    main.js

--- geo-stache.client/src/main.js
+++ geo-stache.client/src/main.js
@@ -7,9 +7,14 @@ import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
 import './utils/SocketProvider.js'
 
+import Vue3Tour from 'vue3-tour'
+
+import 'vue3-tour/dist/vue3-tour.css'
+
 const root = createApp(App)
 registerGlobalComponents(root)
 
 root
   .use(router)
+  .use(Vue3Tour)
   .mount('#app')
