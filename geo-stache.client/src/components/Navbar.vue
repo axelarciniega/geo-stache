f3210a4c5ca3efc2dda1a0bae05600e760d54126
tif <youfoundtiffany@gmail.com>
2023-10-05 11:21:21 -0600

    navbar cleanup

--- geo-stache.client/src/components/Navbar.vue
+++ geo-stache.client/src/components/Navbar.vue
@@ -30,17 +30,13 @@
             About Us
           </router-link>
         </li>
-        <!-- <li>
-          <router-link :to="{ name: 'Stache Details' }" class="btn text-success lighten-30 selectable text-uppercase">
-            StacheDetailsPage
-          </router-link>
-        </li> -->
+
       </ul>
       <!-- MODAL WRAPPER -->
       <div v-if="user.isAuthenticated" class=" p-2 rounded txt-Sand" id="navbarText">
         <ul class="navbar-nav me-auto txt-Sand">
           <ModalWrapper id="id">
-            <!-- ✅DONE add back in when ready -  v-if="user.isAuthenticated" -->
+
             <template #button>
               <i class="mdi mdi-plus-box"></i> Create Stache
             </template>
@@ -52,8 +48,7 @@
 
       </div>
       <div class="collapse navbar-collapse p-2 rounded txt-Sand text-end" id="navbarText" v-else>
-        <!-- <button data-balloon-length="medium" aria-label="Join us in and start your Stache Adventures!"
-          data-balloon-pos="up">Join us in and start your Stache Adventures!"</button> -->
+
 
         <button disabled class="btn txt-Sand text-end">login to create stache</button>
       </div>
@@ -84,17 +79,6 @@ export default {
 </script>
 
 <style scoped>
-/* .logo {
-  height: 70px;
-  width: 70px;
-  filter: drop-shadow (0 0 0.75rem crimson);
-  filter: drop-shadow(3px 8px 7px #292828);
-} */
-
-/* .background-nav {
-  background: linear-gradient(45deg, #41644A, #7da386);
-} */
-
 a:hover {
   text-decoration: none;
 }
@@ -108,11 +92,4 @@ a:hover {
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
 }
-
-/* 
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
-} */
 </style>
